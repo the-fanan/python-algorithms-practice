9041651700b2245a29b24dec506dfb0dcb316b6c
Fanan Dala <fanan.dala@yahoo.com>
2019-09-19 15:32:01 +0100

    intro to graphs

---
+++ graphs-dap.py
@@ -0,0 +1,10 @@
+#Data structures and Algorithms in Python by Goodrich, Tamassia and Goldwasser
+
+# a GRAPH is a way of representing connections or relationships between pairs of objects from some set V
+
+# VERTICE (node) are the objects sharing a relationship
+# EDGE (arc) connect from one vertice to another and may be DIRECTED or UNDIRECTED
+# PATH is a sequence os alternating EDGES and VERTICES. Starts and ends at a VERTICE
+# CYCLE is a PATH that starts and endsat the same VERTICE
+# A PATH is SIMPLE if each VERTICE is didtinct
+# A graph is ACYLIC if it has no DIRECTED CYCLES
